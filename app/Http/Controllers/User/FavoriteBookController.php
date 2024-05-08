8f8af48be835a060c261bdf556bad2619a70424f
ryo-ito1993 <ryo.ito1993@gmail.com>
2024-05-08 16:53:07 +0900

    feat:読みたい一覧の作成

--- app/Http/Controllers/User/FavoriteBookController.php
+++ app/Http/Controllers/User/FavoriteBookController.php
@@ -5,11 +5,28 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Library\googleBooksApiLibrary;
 
 class FavoriteBookController extends Controller
 {
+    public function __construct(
+        protected googleBooksApiLibrary $googleBooksApiLibrary
+    ) {
+    }
+
     public function index(): View
     {
-        return view('user.favorite_books.index');
+        $user = auth()->user();
+        $favoriteBooks = $user->favoriteBooks;
+        $favoriteBooksIsbn = $favoriteBooks->pluck('isbn');
+        $books = [];
+
+        if ($favoriteBooksIsbn->isNotEmpty()) {
+            foreach ($favoriteBooksIsbn as $isbn)
+            {
+                $books[] = $this->googleBooksApiLibrary->getBookByIsbn($isbn);
+            }
+        }
+        return view('user.favorite_books.index', ['books' => $books]);
     }
 }
