9adac5c28b187e5a1ad4028ef5ed853c32486b5f
ryo-ito1993 <ryo.ito1993@gmail.com>
2024-05-10 22:53:36 +0900

    feat:未ログインでレビュー一覧が見れる

--- resources/views/components/parts/user_header.blade.php
+++ resources/views/components/parts/user_header.blade.php
@@ -17,6 +17,9 @@
             <ul class="navbar-nav ms-auto">
                 <!-- Authentication Links -->
                 @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('user.reviews.index') }}">{{ __('レビューリスト') }}</a>
+                    </li>
                     @if (Route::has('login'))
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
