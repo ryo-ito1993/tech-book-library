40410f6b9cc329016cfc063a4e2c3a498be07ec4
ryo-ito1993 <ryo.ito1993@gmail.com>
2024-05-12 10:06:54 +0900

    feat:サイドバー

---
+++ resources/views/components/parts/admin_sidebar.blade.php
@@ -0,0 +1,17 @@
+<div class="container-fluid px-5 my-5">
+    <div class="row justify-content-center">
+        <ul class="list-group list-group-flush accordion" id="accordion">
+            <a href="{{ route('admin.top') }}" class="list-group-item list-group-item-action"><i class="fas fa-users-cog me-1"></i> ホーム</a>
+            <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-users me-1"></i> ユーザー一覧</a>
+            <a href="#" class="list-group-item list-group-item-action"><i class="far fa-comments me-1"></i> レビュー一覧</a>
+            <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-tags me-1"></i> カテゴリ一覧</a>
+            <a class="list-group-item list-group-item-action" href="{{ route('admin.logout') }}"
+                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();"
+                                >
+                <i class="fas fa-sign-out-alt me-1"></i>
+                ログアウト
+            </a>
+        </ul>
+    </div>
+</div>
--- routes/web_admin.php
+++ routes/web_admin.php
@@ -8,10 +8,6 @@ use App\Http\Controllers\Admin\Auth\ForgotPasswordController;
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-// Route::get('password/reset', [ForgotPasswordController::class, 'showAdminLinkRequestForm'])->name('password.request');
-// Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-// Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-// Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
 Route::middleware('auth:admin')->group(static function () {
     // TOP
