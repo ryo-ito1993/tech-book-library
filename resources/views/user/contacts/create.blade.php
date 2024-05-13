ad4f2b02cf75e429cd64afc19c744bdbe152688c
ryo-ito1993 <ryo.ito1993@gmail.com>
2024-05-13 12:22:00 +0900

    feat:お問合せフォームview

---
+++ resources/views/user/contacts/create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('title', 'お問い合わせ')
+
+@section('content')
+    <div class="container" style="max-width: 1000px;">
+        <div>
+            <div class="bg-primary p-3 text-center">
+                <h1 class="h4 m-0 text-white">
+                    お問い合わせ
+                </h1>
+            </div>
+            <div class="px-3 pb-5 pt-3 collapse show">
+                <div class="card-body">
+                    <form method="POST" action="#">
+                        @csrf
+
+                        <div class="form-group row mb-3">
+                            <label for="name" class="form-label">お名前</label>
+                            <input type="text" class="form-control bg-white @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}">
+                            @include('components.form.error', ['name' => 'name'])
+                        </div>
+
+                        <div class="form-group row mb-3">
+                            <label for="email" class="form-label">メールアドレス</label>
+                            <input type="text" class="form-control bg-white @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
+                            @include('components.form.error', ['name' => 'email'])
+                        </div>
+
+                        <div class="form-group row mb-3">
+                            <label for="message" class="form-label">お問い合わせ内容</label>
+                            <textarea class="form-control bg-white @error('message') is-invalid @enderror" id="message" name="message" rows="5" required>{{ old('message') }}</textarea>
+                            @include('components.form.error', ['name' => 'message'])
+                        </div>
+
+                        <div class="text-center mt-4">
+                            <button type="submit" class="btn btn-primary">確認画面へ</button>
+                            <p class="mt-4">ご返信希望のお問い合わせ頂きました内容に関しまして、随時ご回答してまいりたいと思います。お時間を頂く場合もございますことご了承下さい。</p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
