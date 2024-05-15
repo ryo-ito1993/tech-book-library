03a75d0777b90ca9cae833d011719d5c5458e64e
ryo-ito1993 <ryo.ito1993@gmail.com>
2024-05-15 18:49:29 +0900

    feat:レビューコントローラのテスト

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Book;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'book_id' => Book::factory(),
+            'body' => $this->faker->realText(100),
+            'rate' => $this->faker->numberBetween(1, 5),
+        ];
+    }
+}
